096225aef5fa56b803bc8b9f5e99e4ebf2b41537
nerdsrescueme <nerdsrescueme@gmail.com>
2011-07-22 01:16:24 -0400

    Created payment driver BASIC and payment static interface

--- classes/payment.php
+++ classes/payment.php
@@ -20,10 +20,101 @@ namespace Payments;
  *
  * @package    Payments
  * @author     Chase "Syntaqx" Hutchins
+ * @author     Frank Bardon Jr. <nerdsrescueme>
  * @version    1.0
  */
 class Payment {
 
+	protected static $_instance = null;
+
+	public static instance()
+	{
+		if (static::$_instance === null)
+		{
+			static::$_instance = static::factory();
+		}
+		return static::$_instance;
+	}
+
+	/**
+	 * Create a new instance of the payment driver.
+	 *
+	 * @param array configuration
+	 */
+	public static factory($config = array())
+	{
+		$initconfig = Config::load('payments');
+		
+		if (is_array($initconfig) and is_array($config))
+		{
+			$config = array_merge($initconfig, $config);
+		}
+
+		$gateway = ucfirst($config['gateway']);
+		$driver = 'Payment_Driver_' . $gateway;
+		
+		if ( ! class_exists($driver))
+		{
+			throw new \Fuel_Exception("Payment gateway $gateway is not valid.");
+		}
+		
+		return new $class($config);
+	}
+
+	/**
+	 * Set fields needed for payment request
+	 * 
+	 * @param array fields to be added
+	 */
+	public static function set_fields(array $fields)
+	{
+		return static::instance()->set_fields($fields);
+	}
+
+	/**
+	 * Build the request to be sent.
+	 */
+	public static function request()
+	{
+		return static::instance()->request();
+	}
+
+	/**
+	 * Send the request to the payment gateway.
+	 */
+	public static function send()
+	{
+		return static::instance()->send();
+	}
+
+	/**
+	 * Set an error in the errors array
+	 *
+	 * @param integer error code
+	 * @param string  description of error
+	 */
+	public static function set_error(integer $code, string $description)
+	{
+		return static::instance()->set_error($code, $description);
+	}
+
+	/**
+	 * Get an error by its code.
+	 *
+	 * @param integer code requested
+	 */
+	public static function error(integer $code)
+	{
+		return static::instance()->error($code);
+	}
+
+	/**
+	 * Return all errors in an array.
+	 */
+	public static function errors()
+	{
+		return static::instance()->errors();
+	}
 }
 
 /* End of file payment.php */
--- classes/payment/driver.php
+++ classes/payment/driver.php
@@ -16,14 +16,160 @@
 namespace Payments;
 
 /**
- * Payment driver interface
+ * Payment driver
  *
  * @package    Payments
  * @author     Chase "Syntaqx" Hutchins
+ * @author     Frank Bardon Jr. <nerdsrescueme>
  * @version    1.0
  */
-interface Payment_Driver {
+abstract class Payment_Driver {
 
+	protected $fields = array(
+		'card_number' => 0,
+		'card_expires' => 'mm/yy',
+		'card_cvv' => 0,
+		'description' => '',
+		'amount' => 0,
+		'tax' => 0,
+		'shipping' => 0,
+		'name' => array(
+			'first' => '',
+			'last' => '',
+		),
+		'company' => '',
+		'address' => '',
+		'city' => '',
+		'state' => '',
+		'zip' => 0,
+		'ship_to' => array(
+			'name' => array(
+				'first' => '',
+				'last' => '',
+			),
+			'company' => '',
+			'address' => '',
+			'city' => '',
+			'state' => '',
+			'zip' => 0,
+		),
+		'email' => '',
+		'phone' => 0,
+		'fax' => 0,
+	);
+
+	/**
+	 * Which fields are required by this service provider?
+	 *
+	 * @access protected
+	 */
+	protected $required_fields = array();
+
+	/**
+	 * Error message storage.
+	 *
+	 * @access protected
+	 */
+	protected $errors = array();
+
+	/**
+	 * Request body to be sent
+	 *
+	 * @access protected
+	 */
+	protected $_request = null;
+
+
+	/**
+	 * Sets data within the driver to be sent to the service
+	 * provider in order to fulfill the payment request.
+	 *
+	 * @param  array fields to be set
+	 * @return Payment_Driver
+	 */
+	public function set_fields(array $fields)
+	{
+		$this->fields = array_merge($fields, $this->fields);
+		
+		return $this;
+	}
+
+	/**
+	 * Send the request to the payment gateway. It will render
+	 * the request if one is not present.
+	 *
+	 * return boolean
+	 */
+	public function send()
+	{
+		if ( ! $this->_request)
+		{
+			$this->_build_request();
+		}
+		return $this->_send();
+	}
+
+	/**
+	 * The actual send logic
+	 */
+	protected abstract function _send();
+
+	/**
+	 * Build the request string to be sent to the payment gateway.
+	 *
+	 * @return Payment_Driver
+	 */
+	public function request()
+	{
+		$this->_request = $this->_build_request();
+		
+		return $this;
+	}
+
+	/**
+	 * The actual request logic.
+	 */
+	protected abstract function _build_request();
+
+	/**
+	 * Add an error to the errors array
+	 *
+	 * @param  integer error code
+	 * @param  string  description of the error
+	 * @return Payment_Driver
+	 */
+	public function set_error(integer $code, string $description)
+	{
+		$this->errors[$code] = $description;
+		
+		return $this;
+	}
+
+	/**
+	 * Check for an error by code and return its value if
+	 * it exists in the errors array.
+	 *
+	 * @param integer     lookup code
+	 * @return array|null specific error or nothing
+	 */
+	public function error(integer $code)
+	{
+		if (array_key_exists($code, $this->errors))
+		{
+			return $this->errors[$code];
+		}
+		return null;
+	}
+
+	/**
+	 * Return the entire errors array
+	 *
+	 * @return array all errors present
+	 */
+	public function errors()
+	{
+		return $this->errors();
+	}
 }
 
 /* End of file driver.php */
