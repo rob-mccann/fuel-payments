37537d6e46479ba8030cb50af7cc9969837981a2
nerdsrescueme <nerdsrescueme@gmail.com>
2011-07-22 00:12:21 -0400

    Added correct paths in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -19,14 +19,14 @@ Autoloader::add_core_namespace('Payments');
 Autoloader::add_classes(array(
 	'Payments\\Payment'                         => __DIR__.'/classes/payment.php',
 	'Payments\\Payment_Driver'                  => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Authorize'        => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Googlecheckout'   => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Moneybookers'     => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Paypal'           => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Paypalpro'        => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Trident'          => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Trustcommerce'    => __DIR__.'/classes/payment/driver.php',
-	'Payments\\Payment_Driver_Yourpay'          => __DIR__.'/classes/payment/driver.php',
+	'Payments\\Payment_Driver_Authorize'        => __DIR__.'/classes/payment/driver/authorize.php',
+	'Payments\\Payment_Driver_Googlecheckout'   => __DIR__.'/classes/payment/driver/googlecheckout.php',
+	'Payments\\Payment_Driver_Moneybookers'     => __DIR__.'/classes/payment/driver/moneybookers.php',
+	'Payments\\Payment_Driver_Paypal'           => __DIR__.'/classes/payment/driver/paypal.php',
+	'Payments\\Payment_Driver_Paypalpro'        => __DIR__.'/classes/payment/driver/paypalpro.php',
+	'Payments\\Payment_Driver_Trident'          => __DIR__.'/classes/payment/driver/trident.php',
+	'Payments\\Payment_Driver_Trustcommerce'    => __DIR__.'/classes/payment/driver/trustcommerce.php',
+	'Payments\\Payment_Driver_Yourpay'          => __DIR__.'/classes/payment/driver/yourpay.php',
 ));
 
 /* End of file bootstrap.php */
