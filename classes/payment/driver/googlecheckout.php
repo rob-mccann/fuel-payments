fbc73f75bfa9389f8c2ed196962bb3925262e7bb
Frank Bardon Jr <nerdsrescueme@gmail.com>
2011-08-01 14:00:28 -0300

    Began the basics of the google checkout driver (incomplete).

--- classes/payment/driver/googlecheckout.php
+++ classes/payment/driver/googlecheckout.php
@@ -20,10 +20,73 @@ namespace Payments;
  *
  * @package    Payments
  * @author     Chase "Syntaqx" Hutchins
+ * @author     Frank Bardon Jr. <frank@nerdsrescue.me>
  * @version    1.0
  */
 class Payment_Driver_Googlecheckout implements Payment_Driver {
 
+	/**
+	 * Required driver fields to process a transaction
+	 *
+	 * @var   array
+	 */
+	protected $required_fields = array(
+		'ENDPOINT'       => false,
+		'MERCHANT_ID'    => false,
+	);
+
+	/**
+	 * Provided fields values during this execution
+	 *
+	 * @var   array
+	 */
+	protected $fields = array(
+		'ENDPOINT'          => '',
+		'MERCHANT_ID'       => '',
+	);
+
+	/**
+	 * Set configuration options for utilization within the class
+	 *
+	 * @param   array     Configuration, normally passed from the Payments class
+	 */
+	public function __construct(array $config = array())
+	{
+		//$this->curl_config = $config['googlecheckout']['curl_config'];
+
+		parent::__construct($config);
+
+		if($this->test_mode === true)
+		{
+			$this->set_fields(array(
+				'MERCHANT_ID' => $config['googlecheckout']['SANDBOX_MERCHANT_ID'],
+				'ENDPOINT'    => $config['googlecheckout']['SANDBOX_ENDPOINT'],
+			));
+		}
+		else
+		{
+			$this->set_fields(array(
+				'MERCHANT_ID' => $config['googlecheckout']['MERCHANT_ID'],
+				'ENDPOINT'    => $config['googlecheckout']['ENDPOINT'],
+			));
+		}
+
+		$this->set_fields(array(
+			'VERSION'      => $config['googlecheckout']['VERSION'],
+			'CURRENCYCODE' => $config['googlecheckout']['CURRENCYCODE'],
+			'IPADDRESS'    => $_SERVER['REMOTE_ADDR'],
+		));
+	}
+
+	/**
+	 * Run the transaction
+	 *
+	 * @return    boolean
+	 */
+	public function process()
+	{
+		// Do transaction
+	}
 }
 
 /* End of file googlecheckout.php */
